3896c2e8f84f779c0da6b66c9986a3c771f1ee3b
sidinyjunior <sidinyjunior@gmail.com>
2026-01-04 03:01:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'plano-bienal-v8'; 
+const CACHE_NAME = 'plano-bienal-v9'; 
 const DINAMICO_CACHE = 'devocionais-dinamicos-v1';
 
 // Arquivos que o celular baixa "invisivelmente" assim que instala
