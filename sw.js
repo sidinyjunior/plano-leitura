5b7f5a89645782e028c321b723dcbdeb881847f6
sidinyjunior <sidinyjunior@gmail.com>
2026-02-06 06:14:38 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  */
 
 const CACHE_NAME = 'PLANO BIENAL DE LEITURA BÍBLICA & DEVOCIONAL'; 
-const DINAMICO_CACHE = 'VERSAO_V22';
+const DINAMICO_CACHE = 'VERSAO_V25';
 
 // 1. ARQUIVOS PARA BAIXAR IMEDIATAMENTE (CACHE ESTRUTURAL + CONTEÚDO INICIAL)
 const assets = [
@@ -45,7 +45,10 @@ const assets = [
   'https://raw.githubusercontent.com/sidinyjunior/plano-leitura/main/Devocional_Diario/devocional_019.json',
   'https://raw.githubusercontent.com/sidinyjunior/plano-leitura/main/Devocional_Diario/devocional_020.json',
   'https://raw.githubusercontent.com/sidinyjunior/plano-leitura/main/Devocional_Diario/devocional_021.json',
-  'https://raw.githubusercontent.com/sidinyjunior/plano-leitura/main/Devocional_Diario/devocional_022.json'
+  'https://raw.githubusercontent.com/sidinyjunior/plano-leitura/main/Devocional_Diario/devocional_022.json',
+  'https://raw.githubusercontent.com/sidinyjunior/plano-leitura/main/Devocional_Diario/devocional_023.json',
+  'https://raw.githubusercontent.com/sidinyjunior/plano-leitura/main/Devocional_Diario/devocional_024.json',
+  'https://raw.githubusercontent.com/sidinyjunior/plano-leitura/main/Devocional_Diario/devocional_025.json'
 ];
 
 // EVENTO DE INSTALAÇÃO: Acontece quando o app é aberto pela primeira vez ou quando a versão muda
